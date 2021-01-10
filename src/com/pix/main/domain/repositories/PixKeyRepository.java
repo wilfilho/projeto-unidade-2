e232a5a1a6b875a8929bbc17bb95fea79dc69d6d
Vinicius Melo <vinicius.melo@dcomp.ufs.br>
2021-01-10 16:38:54 -0300

    feat: add normalized pixKey e Statement

--- src/com/pix/main/domain/repositories/PixKeyRepository.java
+++ src/com/pix/main/domain/repositories/PixKeyRepository.java
@@ -3,7 +3,7 @@ package com.pix.main.domain.repositories;
 import com.pix.main.domain.models.PixKey;
 
 public interface PixKeyRepository {
-
+     @Serialized ("keyId")
      void addPixKey(PixKey pixKey);
 
      void removePixKey(Integer pixKeyId);
--- src/com/pix/main/domain/repositories/StatementRepository.java
+++ src/com/pix/main/domain/repositories/StatementRepository.java
@@ -2,8 +2,9 @@ package com.pix.main.domain.repositories;
 
 public interface StatementRepository {
 
+     @Serialized ("id")
      void fetchStatementsByAgency(Integer agencyId);
-
+     @Serialized ("idAccountSender")
      void fetchStatementsByClient(Integer clientId);
 
 }
