4d8472ced9b4170e72aabc7d16dcad520ee7bdb2
Wilson Filho <will.flho@gmail.com>
2021-01-10 22:07:27 -0300

    feat: add mapping for PixStorage

--- src/main/java/com/pix/main/Main.java
+++ src/main/java/com/pix/main/Main.java
@@ -1,9 +1,25 @@
 package com.pix.main;
 
+import com.google.gson.Gson;
+import com.pix.main.domain.models.PixStorage;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello");
+    public static void main(String[] args) throws IOException {
+        String content = readFile("src/main/resources/example.json", Charset.defaultCharset());
+        Gson gson = new Gson();
+        PixStorage storage = gson.fromJson(content, PixStorage.class);
+        System.out.println(storage.getBanks().size());
+    }
+
+    static String readFile(String path, Charset encoding) throws IOException {
+        byte[] encoded = Files.readAllBytes(Paths.get(path));
+        return new String(encoded, encoding);
     }
 
 }
--- src/main/java/com/pix/main/domain/models/PixStorage.java
+++ src/main/java/com/pix/main/domain/models/PixStorage.java
@@ -1,12 +1,19 @@
 package com.pix.main.domain.models;
 
+import com.google.gson.annotations.SerializedName;
+
 import java.util.ArrayList;
 
 public class PixStorage {
 
+    @SerializedName("bancos")
     private ArrayList<Bank> banks;
 
+    @SerializedName("extratos")
     private ArrayList<Statement> statements;
+
+    @SerializedName("clientes")
+    private ArrayList<BankClient> clients;
 
     public ArrayList<Bank> getBanks() {
         return banks;
